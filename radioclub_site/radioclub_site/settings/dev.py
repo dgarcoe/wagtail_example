4a55561651ecdd1024ba22df885d88b8d98b9d56
Daniel García Coego <dannn86m@gmail.com>
2026-02-02 17:34:49 +0000

    First web template

---
+++ radioclub_site/radioclub_site/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-h)u1c^fps#8(kktl6los1x^tu-78c)4kqxa#=j4jr(_f_d$kms"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
